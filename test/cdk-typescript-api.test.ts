a97542643c9a47420278486c4224648845f0dbd9
Oleksii <oleksi.rybak@gmail.com>
2022-07-04 22:13:12 +0100

    adding some tests

--- test/cdk-typescript-api.test.ts
+++ test/cdk-typescript-api.test.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
+import { Template, Match, Capture } from 'aws-cdk-lib/assertions';
 import * as CdkTypescriptApi from '../lib/cdk-typescript-api-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
+test('DynamoDB Table and SNS Topic Created', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new CdkTypescriptApi.CdkTypescriptApiStack(app, 'MyTestStack');
@@ -10,8 +10,34 @@ test('SQS Queue and SNS Topic Created', () => {
 
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
   template.resourceCountIs('AWS::SNS::Topic', 1);
+  template.resourceCountIs('AWS::DynamoDB::Table', 1);
 });
+
+test('Lambda Has Environment Variables', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkTypescriptApi.CdkTypescriptApiStack(app, 'MyTestStack');
+
+  // THEN
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual(
+    {
+      Variables: {
+        DYNAMODB_TABLE: {
+          Ref: "ClientDonationsA5467BC3",
+        },
+        SNS_TOPIC_ANR: {
+          Ref: "ClientDonationsTopic931D4A0D",
+        },
+      },
+    }
+  );
+});
+
+

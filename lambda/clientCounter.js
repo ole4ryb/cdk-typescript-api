199748be660f2c07a63abcae621bb5fb23154cf8
Oleksii <oleksi.rybak@gmail.com>
2022-07-04 02:02:15 +0100

    fetching the passed id

--- lambda/clientCounter.js
+++ lambda/clientCounter.js
@@ -7,6 +7,6 @@ exports.handler = async function(event) {
     return {      
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
-      body: `Hello, CDK! You've hit ${event.path}\n`
+      body: `Hello, CDK! You've hit ${event.path} and ${event.pathParameters.id} \n`
     };
   };
